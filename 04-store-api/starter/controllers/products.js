805b035861b4d2b661da5928b18cd76d5a0415d3
Lisa Geist <lbgeist@gmail.com>
2023-07-04 08:53:05 -0400

    final project store-api

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -6,8 +6,8 @@ const getAllProductsStatic = async (req, res) => {
   // const products = await Product.find({
   //   name: { $regex: search, $options: 'i' }, // 'i' = case insensitive
   // });
-  const products = await Product.find({})
-    .sort('name') // finds all products and sorts them by name
+  const products = await Product.find({ price: { $gt: 30 } })
+    .sort('price') // finds all products and sorts them by name
     .select('name price') // only shows specific fields
     .limit(10) // limits number of records shown
     .skip(1); // skips first records by number specified. Use Skip and limit for pagination
@@ -15,7 +15,7 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -27,7 +27,32 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: 'i' };
   }
-  // console.log(queryObject);
+
+  // Converts user-friendly operators to Mongoose specific operators
+  if (numericFilters) {
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<=': '$lte',
+    };
+    const regEx = /\b(<|>|=|<=|>=)\b/g; // need to lookup what this means
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ['price', 'rating']; // will only be able to use numericFilters on these fields
+    filters = filters.split(',').forEach((item) => {
+      const [field, operator, value] = item.split('-');
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+  }
+
+  console.log(queryObject);
+
   let result = Product.find(queryObject);
 
   // sort
@@ -44,8 +69,11 @@ const getAllProducts = async (req, res) => {
     result = result.select(fieldsList);
   }
 
+  // for page functionality
   const page = Number(req.query.page) || 1;
   const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
 
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
