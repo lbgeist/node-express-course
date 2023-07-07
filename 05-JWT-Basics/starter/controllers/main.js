ec1fcb21a01bee9fdb700d9eb431c1486ade1e67
Lisa Geist <lbgeist@gmail.com>
2023-07-07 08:05:29 -0400

    began update but now stuck

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,4 +1,12 @@
+// check username, password in post(login) request
+// if exist create new JWT
+// send back to front-end
+
+// setup authentication so only the request with JWT can access the dashboard
+
 const login = async (req, res) => {
+  const { username, password } = req.body;
+  console.log(username, password);
   res.send('Fake Login/Register/Signup Route');
 };
 
