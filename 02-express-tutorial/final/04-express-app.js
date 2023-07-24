5d75d614550e4c90ef473b7d9647aca113f0c11e
Lisa Geist <lbgeist@gmail.com>
2023-07-24 08:40:26 -0400

    add notes to existing work

--- 02-express-tutorial/final/04-express-app.js
+++ 02-express-tutorial/final/04-express-app.js
@@ -1,19 +1,23 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path'); // comes included with Node.js
 
-const app = express()
+const app = express();
 
 // setup static and middleware
-app.use(express.static('./public'))
+app.use(express.static('./public')); // makes resources in folder available without having to set them up individually
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-})
+  res.sendFile(path.resolve(__dirname, './navbar-app/index.html')); // sendFile requires path could also use path.join
+  // not needed because index.html is a static asset and can be included in the app.use above
+  // use other methods to render index.html:
+  // adding to static assets
+  // server side rendering (SSR) using template engine
+});
 
 app.all('*', (req, res) => {
-  res.status(404).send('resource not found')
-})
+  res.status(404).send('resource not found');
+});
 
 app.listen(5000, () => {
-  console.log('server is listening on port 5000....')
-})
+  console.log('server is listening on port 5000....');
+});
